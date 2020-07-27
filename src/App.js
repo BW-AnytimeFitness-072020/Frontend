306612e3b9db9dd23b2879860bd5131e21f03970
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-07-27 13:58:44 -0400

    imported on app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
+import { Link, Switch, Route } from 'react-router-dom';
+import axios from 'axios'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
