5a6c13b764582cb9897663713bae2546a5b23657
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-07-28 12:49:26 -0400

    seperated states and added axios

--- src/App.js
+++ src/App.js
@@ -1,101 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import * as yup from 'yup';
-import { Link, Switch, Route } from 'react-router-dom';
-import axios from 'axios'
+import React, { useState} from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import SignIn from './SignInPage.jsx';
 import Register from './RegisterPage.jsx';
-import FormSchema from './FormSchema.js';
+import Header from './header.jsx';
 
-const initialSignIn = {
-  username: '',
-  password: '',
-  user: {
-    client: false,
-    instructor: false,
-  },
-}
 
-const initialErrors = {
-  username: '',
-  password: '',
-}
 
-const initialDisabled = true
+
+
+
 
 function App() {
-
-  const [signIn, setSignIn] = useState(initialSignIn)
-  const [disabled, setDisabled] = useState(initialDisabled)
-  const [errors, setErrors] = useState(initialErrors)
-
-  const inputChange = (name, value) => {
-    yup
-      .reach(FormSchema, name)
-      .validate(value)
-      .then(valid => {
-        setErrors({
-          ...errors, [name]: '',
-        })
-      })
-      .catch(error => {
-        setErrors({
-          ...errors, [name]: error.errors[0],
-        })
-      })
-      setSignIn({...signIn, [name]: value})
-  }
-
-  const checkChange = (name, isChecked) => {
-    setSignIn({
-      ...signIn, users: {
-        ...signIn.users, [name]: isChecked,
-      }
-    })
-  }
-
-  const submit = () => {
-    
-  }
-
-  useEffect(() => {
-    FormSchema.isValid(signIn).then(valid => {
-      setDisabled(!valid)
-    }, [signIn])
-  })
+  
 
   return (
     <div className="App">
-      <header>
-        <h1>ANYWHERE FITNESS</h1>
-        <button>
-          <Link to='/'>Home</Link>
-        </button>
-        <button>
-          <Link to='/signin'>Sign In</Link>
-        </button>
-        <button>
-          <Link to='/register'>Register</Link>
-        </button>
-      </header>
+      <Header />
       <Switch>
-        <Route exact path='/signin'>
-          <SignIn 
-          inputchange={inputChange}
-          disabled={disabled}
-          errors={errors}
-          signIn={signIn}
-          checkChange={checkChange}
-          submit={submit}/>
+        <Route path='/signin'>
+          <SignIn />
         </Route>
-        <Route exact path='/register'>
-          <Register 
-          inputchange={inputChange}
-          disabled={disabled}
-          errors={errors}
-          signIn={signIn}
-          checkChange={checkChange}
-          submit={submit}/>
+        <Route path='/register'>
+          <Register />
         </Route>
       </Switch>
      
--- src/ClientForm.jsx
+++ src/ClientForm.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
-
-export default function Client () {
-    
-}
+import ClassCard from './clientClassCard'
+export default function Client (props) {
+    const { user } = props
+    return(
+    <div>
+        <h3> WELCOME {user.name}!</h3>
+        <h5>Please select a class</h5>
+        <div className='search'>
+            <label>
+                <input
+                    name='search'
+                    placeholder='Search Classes'
+                    type='search'
+                />
+            </label>
+            <button className='searchbutton'>Search</button>
+        </div>
+    </div>
+)}
