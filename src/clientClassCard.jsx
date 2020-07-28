e3688d70565dbaba0e5bf8aab088d1cdb4ce4b1c
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-07-28 14:34:44 -0400

    working on client, not complete!

--- src/clientClassCard.jsx
+++ src/clientClassCard.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
-export default function ClassCard () {
+export default function ClassCard ({classes}) {
+    if (!classes) {
+        return <h3>We Are Finding Classes For You</h3>
+    }
    return (
     <div>
     <h4>Available Classes</h4>
-    <p>Course: {coursename} </p>
-    <p>Activity: {type}</p>
-    <p>Start Time: {starttime }</p> 
-    <p>Duration: {duration}</p>
-    <p>Intensity: {intensitylevel}</p>
-    <p>Location: {location}</p>
-     <p>CLass Capacity: {sizecapacty}</p>   
+    <p>Course: {classes.coursename} </p>
+    <p>Activity: {classes.type}</p>
+    <p>Start Time: {classes.starttime }</p> 
+    <p>Duration: {classes.duration}</p>
+    <p>Intensity: {classes.intensitylevel}</p>
+    <p>Location: {classes.location}</p>
+     <p>CLass Capacity: {classes.sizecapacty}</p>   
     </div>
-     
-     
    ) 
 }
