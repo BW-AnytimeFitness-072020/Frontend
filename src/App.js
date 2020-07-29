4183c45d73bc179c8633f4338bd7163f064abac5
Nathan DeSpain <nathandespain@Nathans-MacBook-Pro.local>
2020-07-29 09:55:45 -0700

    Added basic html in InstructorDash to put a list of classes and a form to make new classes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Classes from './components/Classes'
 import SignIn from './SignInPage.jsx';
 import Register from './RegisterPage.jsx';
 import Header from './header.jsx';
+import InstructorForm from './InstructorForm'
 import { UserContext } from './contexts/userContext';
 import './App.css'
 
@@ -15,7 +16,30 @@ import './App.css'
 
 
 const initialUserData = {
+  username: 'Jimmay',
+  email: 'Jimmay@jimmay.com',
+  password: 'Jimmay jr.',
+  joinedclasses: [{
+    coursename: "Zoomba",
+    type: "Cardio",
+    starttime: 12,
+    duration: 50,
+    intensitylevel: "Medium",
+    location: "Florida",
+    sizecapacity: 25
+  }],
+  createdclasses: [{
+    coursename: "Zoomba",
+    type: "Cardio",
+    starttime: 12,
+    duration: 50,
+    intensitylevel: "Medium",
+    location: "Florida",
+    sizecapacity: 25
 
+  }],
+  client: false,
+  instructor: true,
 }
 
 
@@ -25,49 +49,49 @@ function App() {
 
   return (
     <UserContext.Provider value={ userData }>
-          {/* Put Client Page and Route inside */}
-        
-        
-    <Container fluid={true}>
-    <Row>
-    <Col>
-                    Anytime Fitness
-                </Col>
-                <Col>
-                    <Link to='/'>Home</Link> </Col>
-                 <Col>   <Link to='/signin'>Sign In</Link></Col>
-                 <Col>   <Link to='/register'>Register</Link></Col>
-                 <Col>   <Link to='/classes'>Classes</Link></Col>
-                 <Col>   <Link to='/about'>About Us</Link>
-                </Col>
-      
-    {/* <div className="App">
-      <Header /></div> */}
-      </Row>
+      <Container fluid={true}>
       <Row>
       <Col>
-      <Switch>
-        <Route path='/signin'>
-          <SignIn />
-        </Route>
-        <Route path='/register'>
-          <Register />
-        </Route>
+                      Anytime Fitness
+                  </Col>
+                  <Col>
+                      <Link to='/'>Home</Link> </Col>
+                  <Col>   <Link to='/signin'>Sign In</Link></Col>
+                  <Col>   <Link to='/register'>Register</Link></Col>
+                  <Col>   <Link to='/classes'>Classes</Link></Col>
+                  <Col>   <Link to='/about'>About Us</Link>
+                  </Col>
         
-        <Route path='/classes'>
-          <Classes />
-        </Route>
-        <Route path='/about'>
-          {/* <About /> */}
-        </Route>
-        
-        <Route path='/'>
-        <Home />
-        </Route>
-      </Switch>
-      </Col>
-      </Row>
-    </Container>
+      {/* <div className="App">
+        <Header /></div> */}
+        </Row>
+        <Row>
+        <Col>
+        <Switch>
+          <Route path='/signin'>
+            <SignIn />
+          </Route>
+          <Route path='/register'>
+            <Register />
+          </Route>
+          
+          <Route path='/classes'>
+            <Classes />
+          </Route>
+          <Route path='/about'>
+            {/* <About /> */}
+          </Route>
+          <Route>
+            <InstructorForm path='/instructor'/>
+          </Route>
+          
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+        </Col>
+        </Row>
+      </Container>
     </UserContext.Provider>
   )
 }
